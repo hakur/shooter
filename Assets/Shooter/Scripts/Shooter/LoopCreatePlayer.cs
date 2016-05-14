740d32bf6794e57ca8a352daa12afaac686d65f2
喻兴 <yuxing951@hotmail.com>
2016-05-14 20:25:01 +0800

    multi map select

--- Assets/Shooter/Scripts/Shooter/LoopCreatePlayer.cs
+++ Assets/Shooter/Scripts/Shooter/LoopCreatePlayer.cs
@@ -2,29 +2,16 @@
 using System.Collections;
 using UnityEngine.Networking;
 namespace Shooter{
-public class LoopCreatePlayer : NetworkBehaviour {
-	public NetworkManager netm;
+public class LoopCreatePlayer : MonoBehaviour {
+	public NetManager netm;
 	
 	void Awake() {
-		netm = GameObject.FindWithTag("NetworkManager").GetComponent<NetworkManager>();
+		netm = GameObject.FindWithTag("NetworkManager").GetComponent<NetManager>();
 	}
 	void Start()
     {
-        
-        //CmdSpawn();
+        netm.RespawnPlayer();
     }
-	[Command]
-	void CmdSpawn() {
-		Debug.Log("复活");
-		GameObject player = Instantiate(netm.playerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
-		NetworkServer.Spawn(player);
-	}
-	public override void OnStartLocalPlayer()
-     {
-         base.OnStartLocalPlayer();
- Debug.Log("复活111111111111");
-         // you can use this function to do things like create camera, audio listeners, etc.
-         // so things which has to be done only for our player
-     }
+
 }
 }
--- Assets/Shooter/Scripts/Shooter/NetManager.cs
+++ Assets/Shooter/Scripts/Shooter/NetManager.cs
@@ -1,6 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class NetManager : MonoBehaviour {
-	
+public class NetManager : NetworkManager {
+    [HideInInspector]
+    public NetworkConnection conn;
+    [HideInInspector]
+    public short playerControllerId;
+	public override void OnServerAddPlayer(NetworkConnection c, short pcid) {
+        conn = c;
+        playerControllerId = pcid;
+        //Debug.Log();
+        //Transform trans = GetStartPosition();
+        //Vector3 pos = trans.position;
+        //Quaternion rot = trans.rotation;
+        //GameObject obj = GameObject.Instantiate(playerPrefab,pos,rot) as GameObject;
+        
+        //NetworkServer.AddPlayerForConnection( conn, obj, playerControllerId );
+    }
+     public override void OnClientConnect(NetworkConnection conn) {
+         
+     }
+    public void RespawnPlayer() {
+        Transform trans = GetStartPosition();
+        Vector3 pos = trans.position;
+        Quaternion rot = trans.rotation;
+        GameObject obj = GameObject.Instantiate(playerPrefab,pos,rot) as GameObject;
+        NetworkServer.AddPlayerForConnection( conn, obj, playerControllerId );
+    }
 }
